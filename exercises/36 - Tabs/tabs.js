210290dc2b66b68d0fd6823330dc3a7915df4c1b
stormshark <53909021+stormshark@users.noreply.github.com>
2022-02-13 14:27:25 +0300

    tabs control active passive situation

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,25 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons= tabs.querySelectorAll('[role="tab"]');
+const tabPanels= Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick (event) {
+    console.log(event.currentTarget); 
+    // hide the others
+    tabPanels.forEach( panel => {   
+        panel.hidden=true;
+    });
+
+    // mark other unselected
+    tabButtons.forEach(tab => tab.setAttribute('aria-selected',false));
+
+    // mark this selected
+    event.currentTarget.setAttribute('aria-selected',true);
+
+    // show the related panel
+    const id = event.currentTarget.id;
+    const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby')===id);
+    console.log(tabPanel);
+    tabPanel.hidden=false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick)); 
