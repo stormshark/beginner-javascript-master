d4d71bbb40c1feb79a527a89c99bc033bca9dd50
stormshark <53909021+stormshark@users.noreply.github.com>
2022-02-13 22:40:22 +0300

    tenrary example

---
+++ exercises/39- ternaries/test.js
@@ -0,0 +1,20 @@
+
+
+const count= 2;
+let word ; 
+
+if (count ===1) {
+    word ='item';
+}else {
+    word= 'items';
+}
+
+const sentence = `You have ${count} ${word} in your cart`;
+
+
+// tenrary
+// 1. condition  
+// what if true 
+// what if false
+
+    const word = Count ===1 ? 'item' : 'items';
