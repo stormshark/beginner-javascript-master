41d9888557bfaf9a0943d32cec59289ae67ee4b8
stormshark <53909021+stormshark@users.noreply.github.com>
2022-03-19 02:33:12 +0300

    creating shoppingcart

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -1,1 +1,33 @@
-console.log("works")
+
+const addButton =  document.querySelector('.shopping-list button[type="submit"]');
+const enteredItemText =  document.querySelector('.listTextField');
+const shoppingList =  document.querySelector('.list');
+let itemList=[];
+
+addButton.addEventListener('click',createList);
+
+function createList(event) {
+    
+    event.preventDefault();
+    let itemName = enteredItemText.value;
+    
+    const item = {
+    id:Date.now(),
+    name: itemName,
+    complete:false,
+    }
+    
+    itemList.push(item);
+
+    let cleanlist= itemList.map(e => 
+
+        `<li class="shopping-item">
+            <input type="checkbox">
+            <span class="itemName">${e.name}</span>
+            <button class="closeButton">&times</button>   
+        </li>`).join('');
+
+    shoppingList.innerHTML = cleanlist;
+    enteredItemText.value ='';
+
+} 
