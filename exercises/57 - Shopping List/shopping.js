84d2c63af5db86ec1e73b857ff47e2ac5947d3fd
stormshark <53909021+stormshark@users.noreply.github.com>
2022-03-21 09:44:16 +0300

    shopping cart continues

--- exercises/57 - Shopping List/shopping.js
+++ exercises/57 - Shopping List/shopping.js
@@ -4,30 +4,56 @@ const enteredItemText =  document.querySelector('.listTextField');
 const shoppingList =  document.querySelector('.list');
 let itemList=[];
 
+
 addButton.addEventListener('click',createList);
+shoppingList.addEventListener('click',removeAnItem);
+
+retrieveData();
+
 
 function createList(event) {
-    
     event.preventDefault();
     let itemName = enteredItemText.value;
-    
+
     const item = {
-    id:Date.now(),
-    name: itemName,
-    complete:false,
-    }
+        id:Date.now(),
+        name: itemName,
+        complete:false,
+        }
     
     itemList.push(item);
+    display ()
+    replicateData();    
+    enteredItemText.value ='';
+} 
 
-    let cleanlist= itemList.map(e => 
-
+function display () {
+    console.log("buraya geldi")
+    let cleanList= itemList.map(e => 
         `<li class="shopping-item">
             <input type="checkbox">
             <span class="itemName">${e.name}</span>
             <button class="closeButton">&times</button>   
         </li>`).join('');
+    shoppingList.innerHTML = cleanList;
+}
 
-    shoppingList.innerHTML = cleanlist;
-    enteredItemText.value ='';
+function retrieveData() {
+    let retrievedData =  JSON.parse(localStorage.getItem("lsItems"));
+    if(retrievedData) 
+        {itemList.push(...retrievedData);
+          display();
+        }
+}
 
-} 
+function removeAnItem(id) {
+    id.preventDefault();
+    console.log("yeni ekleleneşeş g,rd,");
+    id
+
+    //itemList.find(finder => finder.id === id );
+}
+
+function replicateData(){
+    localStorage.setItem("lsItems",JSON.stringify(itemList));
+}
