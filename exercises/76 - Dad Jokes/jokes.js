993173064660aada20f0aa1d601a89362aaf6808
stormshark <53909021+stormshark@users.noreply.github.com>
2022-05-14 17:47:46 +0300

    fetch api response and custom event

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -8,3 +8,35 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+const paragraph = document.querySelector('.joke');
+const baseEndpoint = 'https://icanhazdadjoke.com/';
+let serviceResponseJoke = '';
+
+
+
+const button= document.querySelector('.getJoke') ; 
+button.addEventListener('click',getTheJoke);
+
+async function getTheJoke () {
+  const prom =  await fetch (baseEndpoint,{
+    headers: {
+      Accept: 'application/json',
+    }
+  }) 
+  let serviceResponse = await prom.json();
+  serviceResponseJoke = serviceResponse.joke;
+  console.log("service log: " + serviceResponseJoke);
+  button.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+button.addEventListener('itemsUpdated', setTheJoke);
+
+
+
+function setTheJoke(e) {
+  e.preventDefault();
+  console.log("içeriye girdi");
+  paragraph.textContent='';
+  paragraph.textContent=serviceResponseJoke;
+}
