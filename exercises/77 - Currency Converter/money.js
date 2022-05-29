84c9ec06436de8151fa6e330102ded6d38c6815e
stormshark <stormshark312@hotmail.com>
2022-05-29 14:43:47 +0300

    currency converter finished

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -37,11 +37,11 @@ const currencies = {
 const currencyFromCombo = document.querySelector('[name="from_currency"]');
 const currencyToCombo = document.querySelector('[name="to_currency"]');
 const currencyKeyValue = Object.entries(currencies);
-
+const to_amountText = document.querySelector('.to_amount');
 
 //fill Events
-currencyFromCombo.addEventListener('change', getCurrencyRates);
-currencyToCombo.addEventListener('change', deneme => console.log("asdsadasdasdasda"));
+//let firstComboResult = currencyFromCombo.addEventListener('change', getCurrencyRates("USD"));
+//currencyToCombo.addEventListener('change', test => console.log("inside second combo"));
 
 
 
@@ -62,14 +62,11 @@ currencyToCombo.innerHTML = currencyList;
 // Call after first one and add it to the URL
 
 // TODO #2 how to pass variable and stop event listener for first time ??????
-async function getCurrencyRates(event) {
-
-event.preventDefault();
-baseCurrency ="USD";
+async function getCurrencyRates(_baseCurrency) {
+baseCurrency =_baseCurrency;
  
 var myHeaders = new Headers();
 myHeaders.append("apikey", "Xys0dskeXw9MJLt1qdtWPGVwq6iu1EwG");
-
 var requestOptions = {
   method: 'GET',
   redirect: 'follow',
@@ -79,13 +76,60 @@ var requestOptions = {
 const requestResult = await fetch(`https://api.apilayer.com/exchangerates_data/latest?base=${baseCurrency}`, requestOptions)
   .catch(error => console.log('error', error));
 
-console.log(await requestResult.json());
+let result = await requestResult.json();
+let conversionRate= await result.rates.TRY;
+console.log("turkish lira rate: "+ conversionRate);
+return conversionRate;
+}
 
+//Calculate based on second on 
+const inputNumber = document.querySelector('input');
+inputNumber.addEventListener('change',calculateCurrencyChange);
+//inputNumber.addEventListener('input',calculateCurrencyChange);
+
+async function calculateCurrencyChange(e) {
+e.preventDefault();
+const enteredValue= e.currentTarget.value;
+const conversionRate = await getCurrencyRates("USD");
+to_amountText.textContent= enteredValue * conversionRate ;
 
 }
-//Calculate based on second on 
-
 
 // result 
+
+
+
+
+
+
+
+
+
+// // events and args should be of type Array
+// function addMultipleListeners(element,events,handler,useCapture,args){
+//   if (!(events instanceof Array)){
+//     throw 'addMultipleListeners: '+
+//           'please supply an array of eventstrings '+
+//           '(like ["click","mouseover"])';
+//   }
+//   //create a wrapper to be able to use additional arguments
+//   var handlerFn = function(e){
+//     handler.apply(this, args && args instanceof Array ? args : []);
+//   }
+//   for (var i=0;i<events.length;i+=1){
+//     element.addEventListener(events[i],handlerFn,useCapture);
+//   }
+// }
+
+// function handler(e) {
+//   // do things
+// };
+
+// // usage
+// addMultipleListeners(
+//     document.getElementById('first'),
+//     ['touchstart','click'],
+//     handler,
+//     false);
 
 
